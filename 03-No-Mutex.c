0bcd3d5b6eccc321f6f17909494e47513b4d5303
johnny261 <johnny.aguirre@udea.edu.co>
2026-02-26 21:19:53 -0500

    Create 03-No-Mutex.c

---
+++ 03-No-Mutex.c
@@ -0,0 +1,39 @@
+
+void task1(void *pvParameters)
+{
+    while (1)
+    {
+        int local = sharedCounter;
+        vTaskDelay(pdMS_TO_TICKS(100)); // Simula trabajo
+        sharedCounter = local + 1;
+
+        ESP_LOGI(TAG, "Task1 incrementó contador a %d", sharedCounter);
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+void task2(void *pvParameters)
+{
+    while (1)
+    {
+        int local = sharedCounter;
+        vTaskDelay(pdMS_TO_TICKS(100)); // Simula trabajo
+        sharedCounter = local + 1;
+
+        ESP_LOGI(TAG, "Task2 incrementó contador a %d", sharedCounter);
+
+        vTaskDelay(pdMS_TO_TICKS(700));
+    }
+}
+
+void app_main(void)
+{
+    xTaskCreate(task1, "Task1", 2048, NULL, 2, NULL);
+    xTaskCreate(task2, "Task2", 2048, NULL, 2, NULL);
+
+    while(1)
+    {
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
